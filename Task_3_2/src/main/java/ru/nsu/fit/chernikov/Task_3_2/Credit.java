b2f95eb564b7dea7e8374934846f205b4a6dc4aa
SpykeX3 <spykex3@gmail.com>
2019-12-06 12:24:05 +0700

    Task_3_2

--- Task_3_2/src/main/java/ru/nsu/fit/chernikov/Task_3_2/Credit.java
+++ Task_3_2/src/main/java/ru/nsu/fit/chernikov/Task_3_2/Credit.java
@@ -1,4 +1,35 @@
 package ru.nsu.fit.chernikov.Task_3_2;
 
-public class Credit {
+class Credit {
+  private boolean isMark;
+  private boolean isFinal;
+  private int mark;
+
+  Credit(boolean cred, boolean _final) {
+    isMark = false;
+    if (cred) {
+      mark = 1;
+    } else {
+      mark = 0;
+    }
+    isFinal = _final;
+  }
+
+  Credit(int _mark, boolean _final) {
+    isMark = true;
+    mark = _mark;
+    isFinal = _final;
+  }
+
+  int getMark() {
+    return mark;
+  }
+
+  boolean isMark() {
+    return isMark;
+  }
+
+  boolean isFinal() {
+    return isFinal;
+  }
 }
