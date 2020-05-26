dd917416d5e2a7dcf027ecdf76b58e087f3e67dd
SpykeX3 <spykex3@gmail.com>
2020-05-26 15:25:35 +0700

    changed build script

--- Task_2_2_1/src/test/java/ru/nsu/fit/chernikov/Task_2_2_1/GameTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/fit/chernikov/Task_2_2_1/GameTest.java
@@ -7,8 +7,4 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class GameTest {
-    @Test public void testAppHasAGreeting() {
-        Game classUnderTest = new Game();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
 }
